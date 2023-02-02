0d69dff177627a62fa7f343ec58a63a3a3acba08
yonatancrespo <yonatancrespo4@gmail.com>
2023-02-02 02:26:32 +0000

    finish The traffic light

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+
+
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
@@ -5,8 +7,11 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
+
+
 //import your own components
-import Home from "./component/home.jsx";
+import{ Home }from "./component/home.jsx";
+
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
